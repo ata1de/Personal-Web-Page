480ad2995c0fd8e0f87faf4c8ff8eaf93fa63257
unknown <teteulima@outlook.com>
2023-11-17 15:08:18 -0300

    changed the color and start of button to change light version

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const icon_theme = document.querySelector(".img_theme")
 const html = document.documentElement
+const nav = document.querySelector(".nav_container")
+const menu_icon = document.querySelector(".menu-img")
 
 function tooggleTheme(){
     html.classList.toggle("light")
     if (html.classList.contains("light")){
+        icon_theme.style.width = '30px'
         icon_theme.setAttribute("src", "assets/icons8-crescent-moon-50.png")
     } else {
-        icon_theme.setAttribute("src", "assets/icons8-sun-50.png")
+        icon_theme.setAttribute("src", "assets/icons8-sun-50 white.png")
     }
     
 }
+
+menu_icon.addEventListener("click", () => {
+    nav.classList.toggle("show-menu")
+}
+)
+
 icon_theme.addEventListener("click", () =>{
     tooggleTheme()
 })
